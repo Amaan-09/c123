dfef2da316c186a292d5a2acd1cf1d1cd94c11f9
Amaan-09 <79093439+Amaan-09@users.noreply.github.com>
2021-10-08 18:38:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX=0;
+noseY=0;
+difference = 0;
+leftWristX=0;
+rightWristX=0;
 function setup()
 {
     video= createCapture(VIDEO);
@@ -19,10 +24,22 @@ function gotPoses(results)
 {
 if(results.length > 0){
     console.log(results);
+    noseX= results[0].pose.nose.x;
+    noseY= results[0].pose.nose.y;
+    console.log("noseX = " + noseX + "noseY = "+ noseY);
+
+    leftWristX= results[0].pose.leftWrist.x;
+    rightWristX= results[0].pose.rightWrist.x;
+    difference= floor(leftWristX-rightWristX);
+    console.log("leftWristX = "+leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
 }
 }
 
 function draw()
 {
-    background('#524a4a');
+    background('#969A97');
+    document.getElementById("square_side").innerHTML="Width and height of the square will be = "+ difference+ "px";
+    fill('#F90093');
+    stroke('#F90093');
+    square(noseX, noseY, difference)
 }
